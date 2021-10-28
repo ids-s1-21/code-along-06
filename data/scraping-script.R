7d142341b0f40a858314feabd04ce129645aed5f
Alex Homer <alexander.homer@ed.ac.uk>
2021-10-28 14:42:52 +0000

    State at end of live code-along

--- data/scraping-script.R
+++ data/scraping-script.R
@@ -1,11 +1,21 @@
-library(magrittr)
 library(tidyverse)
 library(httr)
 library(jsonlite)
 
 res <- GET(
-  "https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Countries_in_Europe&cmlimit=500&format=json"
+  "https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Countries_in_Europe&cmlimit=100&format=json"
 )
-res$content
 
-#The rest of this file will be filled in during the live code-along on Thursday.
+temp <- res$content %>%
+  rawToChar() %>%
+  fromJSON()
+
+europe_data_initial <- temp$query$categorymembers
+
+europe_data <- europe_data_initial %>%
+  as_tibble() %>%
+  filter(
+    !str_starts(title, pattern = "Category:"),
+    !(title %in% c("Post-Soviet states", "European microstates"))
+  ) %>%
+  mutate(url_title = str_replace_all(title, pattern = " ", replacement = "_"))
